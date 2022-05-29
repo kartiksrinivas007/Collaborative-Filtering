66e15188f18d4785afb5b2ec1c1c67682236cf9e
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-05-29 15:31:30 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,115 @@
+from crypt import methods
+from flask import Flask,render_template,jsonify,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+import numpy as np
+import model as cf
+from IPython.display import display
+
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+rating_list = list(np.zeros(len(cf.final_ratings.index.tolist())))
+items = cf.final_ratings.index.tolist()
+items_selected =[]
+id_selected =[]
+count = 0
+
+
+
+
+# class MovieDatabase(db.Model):
+#     movie_Id = db.Column(db.Integer(),nullable=False,primary_key=True)
+#     movie_number= db.Column(db.Integer(),nullable=False)
+#     movie_name= db.Column(db.String(length=50),nullable=False,unique=False)
+
+#     def __repr__(self):
+#         return f"{self.movie_name}"
+
+
+
+
+@app.route("/")
+def show_home():
+    # print(cf.final_ratings.index.tolist())
+    return render_template("index.html",items = items)
+
+@app.route("/predictions",methods=["GET","POST"])
+def show_predictions():
+    show_details = 0
+    if request.method == "POST":
+        show_details=1
+        movieID=request.form["movieID"]
+        print(int(movieID))
+        recomm,sim_indices,sim_distances = cf.return_similar_movie(int(movieID))
+        movieselected = items[int(movieID)]
+        print(movieselected)
+        print(sim_indices)
+        items_closest = []
+        distances_closest = []
+        id_closest =[]
+        for i in range(len(sim_indices)):
+            id_closest.append(sim_indices[i])
+            items_closest.append(items[sim_indices[i]])
+            distances_closest.append(sim_distances[i])
+        print(items_closest)
+        return render_template("predictions.html",items = items_closest, distances = distances_closest, id = id_closest, movieselected = movieselected, show_details = show_details)
+    else:
+        items_closest = []
+        id_closest= []
+        distances_closest=[]
+        movieselected=""
+        return render_template("predictions.html",items = items_closest, distances = distances_closest, id = id_closest, movieselected = movieselected, show_details = show_details)
+
+
+@app.route('/ratings',methods=["GET","POST"])
+def show_ratings():
+    # return "this is the ratings page"
+    if request.method == "POST":
+        movieID=request.form["movieID"]
+        rating=request.form["rating"]
+        items_selected.append(items[int(movieID)])
+        id_selected.append(int(movieID))
+        display(cf.final_ratings)
+        print(movieID)
+        print(rating)
+        print("items = ",items_selected)
+        rating_list[int(movieID)]=float(rating)
+        print("value is = ",rating_list[int(movieID)])
+        return redirect("/ratings")
+    else: 
+        return render_template("ratings.html",items= items_selected, rating_list = rating_list,id = id_selected)
+
+@app.route("/clear")
+def clear():
+    global count
+    global rating_list
+    global items_selected
+    global id_selected
+    count = count + 1
+    rating_list = list(np.zeros(len(cf.final_ratings.index.tolist())))
+    items_selected =[]
+    id_selected =[]
+    try:
+        cf.final_ratings.drop(611,inplace=True,axis = 1)
+    except:
+        pass
+    return redirect("/ratings")
+
+@app.route('/ratingspredictions')
+def show_ratingspredictions():
+    # return "this is the ratings predictions page"
+    print(np.where(np.array(rating_list) > 0))
+    print(len(rating_list))
+    cf.addUser(rating_list)
+    display(cf.final_ratings)
+    cf.CF_engine(611,5,10)
+    sorted_movies = cf.print_recommendations(611,10)
+    sorted_movie_names = [i[0] for i in sorted_movies]
+    sorted_movie_ratings = [i[1] for i in sorted_movies]
+    sorted_movie_ids = [i[2] for i in sorted_movies]
+    sorted_movie_items = []
+    clear()
+    return render_template("ratingspredictions.html",items = sorted_movie_names,ratings = sorted_movie_ratings,id = sorted_movie_ids)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,152 @@
+import pandas as pd 
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+#import the dataset!
+ratings = pd.read_csv('ratings.csv', usecols=['userId','movieId','rating'])
+movies = pd.read_csv('movies.csv',usecols=['movieId','title','genres'])
+
+#merge the two datasets to obtain movies and ratings onto the same pandas dataframe !
+merged_ratings = pd.merge(ratings, movies, how='inner', on='movieId')
+final_ratings = merged_ratings.pivot_table(index = 'title',columns='userId',values='rating',fill_value=0)
+
+#this copy will hold the predicted ratings in later stages !
+final_ratings_copy = final_ratings.copy()
+
+
+#The viewer of the page needs to be added !
+def addUser(rating_list):
+    length = len(final_ratings.columns.tolist())
+    final_ratings[(length+1)] = rating_list
+    final_ratings_copy[(length+1)] = rating_list
+
+ # a function to calculated the pariwise distances between EVERY movie and every other movie , we use Scikit -KNN to calculate the distances snce it uses HIGHLY
+def fit_no_params(k_int):
+    k = int(k_int)
+    knn = NearestNeighbors(metric = 'cosine', algorithm='brute')
+    knn.fit(final_ratings.values)
+    differences,movie_indices = distances, indices = knn.kneighbors(final_ratings.values, n_neighbors=k)
+    return differences,movie_indices
+
+
+
+
+def print_recommendations(user,num_recommended_movies):
+  print('The list of the Movies {} Has Watched \n'.format(user))
+  
+
+  for m in final_ratings[final_ratings[user] > 0][user].index.tolist():
+    print(m)
+  
+  print('\n')
+
+  recommended_movies = []
+
+  for m in final_ratings[final_ratings[user] == 0].index.tolist():
+    index_df = final_ratings.index.tolist().index(m)
+    predicted_rating = final_ratings_copy.iloc[index_df, final_ratings_copy.columns.tolist().index(user)]
+    recommended_movies.append((m, predicted_rating,index_df))
+
+  sorted_rm = sorted(recommended_movies, key=lambda x:x[1], reverse=True)
+  
+  print('The list of the Recommended Movies \n')
+  rank = 1
+  for recommended_movie in sorted_rm[:num_recommended_movies]:
+    
+    print('{}: {} - predicted rating:{}'.format(rank, recommended_movie[0], recommended_movie[1]))
+    rank = rank + 1
+
+  return sorted_rm[:num_recommended_movies]  
+
+def CF_engine(userID,k,numrec):
+    knn_obj = NearestNeighbors(metric = 'cosine',algorithm='brute')
+    knn_obj.fit(final_ratings.values)
+    differences,movie_indices = knn_obj.kneighbors(final_ratings.values,n_neighbors=k)
+    print(final_ratings)
+    user_index=final_ratings.columns.tolist().index(userID)
+    
+
+    for movie,title in list(enumerate(final_ratings_copy.index)):
+        if(final_ratings_copy.iloc[movie,user_index] == 0):
+            similar_movie_indices = movie_indices[movie].tolist()
+            similar_movie_distances = differences[movie].tolist()
+
+            if movie in similar_movie_indices:
+                movieId = similar_movie_indices.index(movie)
+                similar_movie_indices.remove(movie)
+                similar_movie_distances.pop(movieId) 
+
+            # However, if the percentage of ratings in the dataset is very low, there are too many 0s in the dataset. 
+            # Some movies have all 0 ratings and the movies with all 0s are considered the same movies by NearestNeighbors(). 
+            # Then,even the movie itself cannot be included in the indices. 
+            # For example, indices[3] = [2 4 7] is possible if movie_2, movie_3, movie_4, and movie_7 have all 0s for their ratings.
+            # In that case, we take off the farthest movie in the list. Therefore, 7 is taken off from the list, then indices[3] == [2 4].
+            else:
+                similar_movie_indices = similar_movie_indices[:k-1]
+                similar_movie_distances = similar_movie_distances[:k-1]
+                
+            # movie_similarty = 1 - movie_distance    
+            closeness = [1-x for x in similar_movie_distances]
+            closeness_copy = closeness.copy()
+            weighted_sum = 0
+
+        # for each similar movie
+            for s in range(0, len(closeness)):
+            
+            # check if the rating of a similar movie is zero
+                if final_ratings_copy.iloc[similar_movie_indices[s], user_index] == 0:
+                    # if the rating is zero, ignore the rating and the similarity in calculating the predicted rating
+                    if len(closeness_copy) == (k - 1):
+                        closeness_copy.pop(s)
+                    
+                    else:
+                        closeness_copy.pop(s-(len(closeness)-len(closeness_copy)))
+
+                # if the rating is not zero, use the rating and similarity in the calculation
+                else:
+                    weighted_sum = weighted_sum + closeness[s]*final_ratings_copy.iloc[similar_movie_indices[s],user_index]
+
+                # check if the number of the ratings with non-zero is positive
+                if len(closeness_copy) > 0:
+                
+                # check if the sum of the ratings of the similar movies is positive.
+                    if sum(closeness_copy) > 0:
+                        pred_rating = weighted_sum/sum(closeness_copy)
+
+                    # Even if there are some movies for which the ratings are positive, some movies have zero similarity even though they are selected as similar movies.
+                    # in this case, the predicted rating becomes zero as well  
+                    else:
+                        pred_rating = 0
+
+                # if all the ratings of the similar movies are zero, then predicted rating should be zero
+                else:
+                    pred_rating = 0
+
+    # place the predicted rating into the copy of the original dataset
+            final_ratings_copy.iloc[movie,user_index] = pred_rating
+    #print_recommendations(userID,numrec)
+
+def return_similar_movie(movie_index):
+    # movie_index = final_ratings.index.tolist().index(title)
+    differences,movie_indices = fit_no_params(5)
+    similar_movie_indices = movie_indices[movie_index].tolist()
+    similar_movie_distances = differences[movie_index].tolist()
+    k=""
+    # movie_pos = similar_movie_indices.index(movie_index) # find the index of the movie itself , since it is the closest neighbour this will always be 0 
+    # similar_movie_indices.remove(movie_index) # remove the movie itself in indices
+    # similar_movie_distances.pop(movie_pos) # remove the movie itself in distances
+    #There are tremendously special cases where nearly all the ratings tend to zero, i.e. nobody has rated these movies. this try bloack handles these special cases
+    try:
+        movie_pos = similar_movie_indices.index(movie_index) # find the index of the movie itself , since it is the closest neighbour this will always be 0 
+        similar_movie_indices.remove(movie_index) # remove the movie itself in indices
+        similar_movie_distances.pop(movie_pos)
+    except:
+        pass
+
+
+    print("The Movies that are quite Similar to " , final_ratings.index[movie_index])
+    for i in similar_movie_indices:
+        movie_title = final_ratings.index[i]
+        print(str(i+1) + ": " + movie_title) 
+        k = k + str(i+1) + ": " + movie_title+ "<br>"
+    return k,similar_movie_indices,similar_movie_distances
